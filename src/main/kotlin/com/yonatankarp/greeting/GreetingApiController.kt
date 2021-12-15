c4beda51e4323ed648de56340247db437e307aa2
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2021-12-15 15:39:12 +0100

    Implement API

---
+++ src/main/kotlin/com/yonatankarp/greeting/GreetingApiController.kt
@@ -0,0 +1,19 @@
+package com.yonatankarp.greeting
+
+import com.yonatankarp.openapi.GreetingApi
+import com.yonatankarp.openapi.models.GreetResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingApiController : GreetingApi {
+
+    override fun greetName(
+        @RequestParam(value = "name", required = false) name: String?
+    ): ResponseEntity<GreetResponse> =
+        if (name.isNullOrBlank())
+            ResponseEntity.ok(GreetResponse("Hello, world!"))
+        else
+            ResponseEntity.ok(GreetResponse("Hello, $name!"))
+}
