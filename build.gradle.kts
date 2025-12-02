b49cd282da57e1699d5dd203686d5e0faa41e5eb
Yonatan Karp-Rudin <yonvata@gmail.com>
2025-12-02 08:55:37 +0100

    Make code compile after migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 }
 
 tasks.withType<Test> {
